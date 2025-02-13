ef9b5b643f36d088f80c4b947b6c111e0d5ae692
Nail Girfanov <nail.ildarovich@gmail.com>
2025-02-13 17:33:54 +0500

    Project preparation

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
 install:
 	uv sync
+
 dev:
 	uv run flask --debug --app page_analyzer:app run
+
+build:
+	./build.sh
+	
+render-start:
+	gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,8 @@
 import os
 
 from flask import Flask, flash, redirect, render_template, request, url_for
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")

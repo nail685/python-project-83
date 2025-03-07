0578fcf493987153b7f361315c5118642b77481b
Nail Girfanov <nail.ildarovich@gmail.com>
2025-03-07 16:29:19 +0500

    update  project

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -6,3 +6,15 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+
+@app.route('/')
+def index():
+    return render_template(
+        '/index.html',
+    )
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template(
+        '/error.html') , 404
